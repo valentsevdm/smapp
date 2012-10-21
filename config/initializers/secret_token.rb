7e769b086a7dcb37146b354fd8dc11a4ebc0543d
valentsevdm <valentsev_dm@mail.ru>
2012-10-21 22:45:48 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smapp::Application.config.secret_token = 'a95d4734557947cb6db9ef8a647eeae89bf8e313bcd5352209b293c7445e38ad9dd89888c33901446be09953229a7a32ebc2df7f595b3119910193f4f69b8e5d'
